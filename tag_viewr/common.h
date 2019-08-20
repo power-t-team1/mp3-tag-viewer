0c0e53f47cef2394e7a9247fe17ea8ad2d85a0b6
SHUBHAM <shubhamkumar50@yahoo.com>
2019-08-21 01:04:58 +0530

    Add files via upload

--- tag_viewr/common.h
+++ tag_viewr/common.h
@@ -8,6 +8,11 @@
 #include <string.h>
 #include <math.h>
 
+struct tags // Structure to store tagging information. 
+{ 
+	char title[100], artist[100], album[100], 
+		track_num[100], year[100], genre[100]; 
+}tat; 
 enum status
 {
 	FAILURE,
@@ -18,5 +23,5 @@ enum status
 
 //Declare Prototypes
 int edit(char *origin_fil, char *action, char *change);
-
+void merge_file(char* file1, char* file2, char* file3);
 #endif
--- tag_viewr/edit.c
+++ tag_viewr/edit.c
@@ -2,7 +2,7 @@
 
 int edit(char *origin_fil, char *action, char *change)
 {
-	char * act;
+	char * act = malloc(sizeof(char) * 10);
 	if(action[0] != '-')
 	{
 		return FAILURE;
@@ -11,37 +11,67 @@ int edit(char *origin_fil, char *action, char *change)
 	{
 		case 't':
 		{
-			printf("tit2\n");
-			act = "TIT2";
+			strcpy(act,"TIT2");
+			//printf("ll%s\n", act);
+			//act = "TIT2";
 			break;
 		}
 		default:
 		{
-			;
+			printf("Enter a valid action\n");
+			exit(0);
+
 		}
 	}
 	FILE *f1, *f2, *f3;
+
 	char *temp = malloc(sizeof(char) * 30);
-	char *buffer;
-	char file1[100] = "k.txt", file2[100], file3[100] = "k.mp3";
-	f1 = fopen(file1, "ab");
+	char *buffer = malloc(sizeof(char) * 30);
+	char file1[100] = "k.mp3", file2[100], file3[100] = "k.mp3";
+
+	f1 = fopen(file1, "wb");
 	f2 = fopen(origin_fil, "rb");
-	char *length;
-	int size;
-	unsigned char pad1[7] = { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x76 }; 
+
+	char length;
+	char lam;// = malloc(sizeof(char));;
+	int size, ch;
+	unsigned char pad1[7] = { 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; 
 	unsigned char pad2[3] = { 0x00, 0x00, 0x00 }; 
-	printf("sksksks\n");
+
+	//printf("sksksks\n");
+	printf("hh%s\n", act);
+
 	fprintf(f1, "ID3"); 
 	fwrite(pad1, sizeof(pad1), 1, f1);
 	fseek(f2,10,SEEK_SET);//went to 10th
-	
+	int count = 8;
 	while(1)
 	{
+		printf("\nMajorBEF%ld",ftell(f2));
 		printf("enter\n");
 		fread(buffer, 1, 4, f2);
-		fseek(f2,-4,SEEK_CUR);//go 4 bytes back from cur pos
+
+		printf("buf:%s\n",buffer );
+		printf("act:%s\n",act );
+		
+		printf("\nbef%ld",ftell(f2));
+		fseek(f2, 3, SEEK_CUR);//next three dots
+		printf("\naft%ld",ftell(f2));
+		//fscanf(f2, "%s", &lam);//get the size from original
+		fread(&lam, 1, 1, f2);
+		printf("\naft%ld",ftell(f2));
+		printf("lam:%c\n",lam);
+		printf("lamINT:%d\n",lam);
+		//fread(length, 1, 1, f2);
+		//fwrite(f1, 1, 10, f2);//atoi(length) + 3, f2);	
+	
+		fseek(f2,-8,SEEK_CUR);//go 4 bytes back from cur pos
+		
+		printf("\naft%ld",ftell(f2));
+		printf("das\n");
 		if(strcmp(buffer, act) == 0)
 		{
+			printf("bufINS:%s\n",buffer );
 			printf("match\n");
 			//fseek(f2,-4,SEEK_CUR);//go 4 bytes back from cur pos
 			printf("\nEnter the track number of the mp3.\n"); 
@@ -63,22 +93,43 @@ int edit(char *origin_fil, char *action, char *change)
 
 			// Adding tag number entered by the user. 
 			fprintf(f1, "%s", temp); 
-
-			fseek(f2, size + 10, SEEK_CUR);
-
+			//printf("\nINSaft%ld",ftell(f1));
+			//fseek(f2, size + 10, SEEK_CUR);
+			fseek(f2, lam + 10, SEEK_CUR);
+			//printf("\nINSaft1%ld",ftell(f2));
+			break;
 		}
 		else
 		{
-			printf("else\n");
-			fwrite(f1, 1, 7, f2);
-			fscanf(f2, "%hhd", length);
-			//fread(length, 1, 1, f2);
+			//printf("else\n");
+			printf("\nf1 else: %ld",ftell(f1));
+			printf("\nf2 else: %ld",ftell(f2));
+			char *buff1 = malloc(sizeof(char) * 30);
+			fread(buff1, 1, 7, f2);
+			fwrite(buff1, 1, 7, f1);
+			//fseek(f2, 7,SEEK_CUR);
+
+			printf("\naftEls%ld",ftell(f2));
+			//fscanf(f2, "%hhd", length);
+			fread(&length, 1, 1, f2);
 			fseek(f2, -1, SEEK_CUR);
 				printf("kkkk\n");
-			fwrite(f1, 1, 10, f2);//atoi(length) + 3, f2);
+			//fwrite(f1, 1, 10, f2);//atoi(length) + 3, f2);
+			printf("len:%c\n",length);
+			printf("lenINT:%d\n",length);
+			char *buff2 = malloc(sizeof(char) * 30);
+			fread(buff2, 1, length + 3, f2);
+			fwrite(buff2, 1, length + 3, f1);
+			//fseek(f2, length + 3,SEEK_CUR);
 			//fwrite(pad2, sizeof(pad2), 1, f1);
 		}
+		count --;
 	}
+	while ((ch = fgetc(f2)) != EOF) // Appending tagging info. 
+	{
+		fputc(ch, f1); 
+	}
+
 	fclose(f1);
 	fclose(f2);
 	fclose(f3);
